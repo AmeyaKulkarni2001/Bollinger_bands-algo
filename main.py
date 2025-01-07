32c2b4ae1a8099e8896fce9730729bce4ccc988f
ameya kulkarni <ameya.2000k@gmail.com>
2025-01-07 22:46:33 +0530

    starting main code

--- main.py
+++ main.py
@@ -282,7 +282,7 @@ def start_bot():
 
 # Run the bot
 if __name__ == "__main__":
-    # threading.Thread(target=scalping_bot, daemon=True).start()
+    threading.Thread(target=scalping_bot, daemon=True).start()
     port = int(os.getenv("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
 
